59cc9fe95b18aad9cc37a3752ed7b2206bb380d2
Tiago Floriano <tiago_damg@hotmail.com>
2023-05-17 12:35:06 -0300

    first commit

---
+++ Pass generator.py
@@ -0,0 +1,9 @@
+import random # Random é a lib responsável por embaralhar os caracteres
+import string
+
+minusculos = string.ascii_lowercase
+maiusculos = string.ascii_uppercase
+numeros = string.digits
+simbolos = string.punctuation
+
+input("Qual o tamanho da senha desejada?")
